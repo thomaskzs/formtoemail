2f0cb08fb72bbbeae8ad1a90c363fb9a7026c102
thomaskzs <83393128+thomaskzs@users.noreply.github.com>
2024-05-09 10:07:09 +0300

    Delete form-to-email directory

--- form-to-email/js/script.js
+++
@@ -1,4 +0,0 @@
-num1 = Math.floor((Math.random() * 5) + 1);
-num2 = Math.floor((Math.random() * 5) + 1);
-document.getElementById('captcha').innerHTML = num1 + ' + ' + num2 + ' = ';
-document.getElementsByName('captcha-solution')[0].value = num1 + num2;
